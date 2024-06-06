d41929bee4b6916dc7ebc760aa4c25a84a88547e
Gonzalo <gonzalo040903@gmail.com>
2024-06-06 18:39:41 -0300

    cambios en el diseño

--- compartuc/src/componentes/carrouselProd/carrouselProd.jsx
+++ compartuc/src/componentes/carrouselProd/carrouselProd.jsx
@@ -1,49 +1,46 @@
 import React, { useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import './styleCarrouselProd.css';
-import { Pagination, Navigation } from 'swiper/modules';
+import { Navigation } from 'swiper/modules';
 
 export function CarrouselProd() {
     return (
         <>
-          <Swiper
-            slidesPerView={1}
-            spaceBetween={15}
-            navigation={true}
-            pagination={{
-              clickable: true,
-            }}
-            breakpoints={{
-              '@0.00': {
-                slidesPerView: 1,
-                spaceBetween: 10,
-              },
-              '@0.75': {
-                slidesPerView: 2,
-                spaceBetween: 20,
-              },
-              '@1.00': {
-                slidesPerView: 3,
-                spaceBetween: 40,
-              },
-              '@1.50': {
-                slidesPerView: 4,
-                spaceBetween: 50,
-              },
-            }}
-            modules={[Pagination, Navigation]}
-            className="mySwiper"
-          >
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 5</SwiperSlide>
-            <SwiperSlide>Slide 6</SwiperSlide>
-            <SwiperSlide>Slide 7</SwiperSlide>
-            <SwiperSlide>Slide 8</SwiperSlide>
-            <SwiperSlide>Slide 9</SwiperSlide>
-          </Swiper>
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={15}
+                navigation={true}
+                breakpoints={{
+                    '@0.00': {
+                        slidesPerView: 1,
+                        spaceBetween: 10,
+                    },
+                    '@0.75': {
+                        slidesPerView: 2,
+                        spaceBetween: 10,
+                    },
+                    '@1.00': {
+                        slidesPerView: 3,
+                        spaceBetween: 10,
+                    },
+                    '@1.50': {
+                        slidesPerView: 4,
+                        spaceBetween: 10,
+                    },
+                }}
+                modules={[Navigation]}
+                className="mySwiper carrouselProd"
+            >
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 5</SwiperSlide>
+                <SwiperSlide>Slide 6</SwiperSlide>
+                <SwiperSlide>Slide 7</SwiperSlide>
+                <SwiperSlide>Slide 8</SwiperSlide>
+                <SwiperSlide>Slide 9</SwiperSlide>
+            </Swiper>
         </>
-      );
+    );
 }
