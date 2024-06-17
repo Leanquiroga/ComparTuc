d753b0068ad84cd882a654806cbfd5ddc25bef74
Gonzalo <gonzalo040903@gmail.com>
2024-06-17 20:57:58 -0300

    navbar termindo

--- compartuc/src/componentes/navbar/navbar.jsx
+++ compartuc/src/componentes/navbar/navbar.jsx
@@ -13,16 +13,14 @@ import {
   MDBInput
 } from 'mdb-react-ui-kit';
 
-
-export default function Navbar() {
+export function Navbar() {
   const [openNav, setOpenNav] = useState(false);
 
   return (
-    <MDBNavbar expand='lg' light bgColor='light'>
+    <MDBNavbar expand='lg' light bgColor='light' className='shadow-0 navbar'>
       <MDBContainer fluid>
         <MDBNavbarBrand href='#'>
           <img src={require("../../images/icono.jpg")} alt="" className='iconoLogo' />
-          ComparTuc
         </MDBNavbarBrand>
         <MDBNavbarToggler
           type='button'
@@ -32,43 +30,250 @@ export default function Navbar() {
         >
           <MDBIcon icon='bars' fas />
         </MDBNavbarToggler>
+
         <MDBCollapse navbar open={openNav}>
-          <MDBNavbarNav id='navbar'>
+          <MDBNavbarNav>
             <div className="seccionNavegacion">
               <MDBNavbarItem>
-                <MDBNavbarLink active aria-current='page' href='#' className="mx-3 textoNavbar">
+                <MDBNavbarLink active aria-current='page' href='#' className="mx-4 textoNavbar borderBtn">
                   Inicio
                 </MDBNavbarLink>
               </MDBNavbarItem>
-
               <MDBNavbarItem>
-                <MDBNavbarLink active aria-current='page' href='#' className="mx-3 textoNavbar">
+                <MDBNavbarLink active aria-current='page' href='#' className="mx-4 textoNavbar">
                   Contacto
                 </MDBNavbarLink>
               </MDBNavbarItem>
-
               <MDBNavbarItem>
-                <MDBNavbarLink active aria-current='page' href='#' className="mx-3 textoNavbar">
+                <MDBNavbarLink active aria-current='page' href='#' className="mx-4 textoNavbar">
                   Nosotros
                 </MDBNavbarLink>
               </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#' className="mx-4 textoNavbar">
+                  Mi cuenta
+                </MDBNavbarLink>
+              </MDBNavbarItem>
             </div>
-            <MDBNavbarItem>
-              <MDBInput label="Busca Productos" contrast  labelStyle={{color: 'grey'}}  type="text" className='textoInputNavbar pe-5 mt-1'>
-                <i className="fas fa-arrow-circle-right trailing px-2"></i>
-              </MDBInput>
-            </MDBNavbarItem>
 
-            <MDBNavbarItem>
+            <div className="seccionInput">
+              <MDBNavbarItem>
+                <MDBInput label="Busca Productos" contrast labelStyle={{ color: 'grey' }} type="text" className='textoInputNavbar pe-5 mt-1'>
+                  <i className="fas fa-magnifying-glass trailing px-2"></i>
+                </MDBInput>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#'>
+                  <MDBIcon far icon="heart mt-3 ms-3" size="lg" />
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#'>
+                  <MDBIcon fas icon="shopping-cart mt-3 ms-3" size='lg' />
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+            </div>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
+}
 
-            </MDBNavbarItem>
+export function NavbarNosotros() {
+  const [openNav, setOpenNav] = useState(false);
 
-            <MDBNavbarItem>
-              <MDBNavbarLink active aria-current='page' href='#'>
-                <MDBIcon far icon="user" className='mx-3'size="lg"/>
-              </MDBNavbarLink>
-            </MDBNavbarItem>
+  return (
+    <MDBNavbar expand='lg' light bgColor='light' className='shadow-0 navbar'>
+      <MDBContainer fluid>
+        <MDBNavbarBrand href='#'>
+          <img src={require("../../images/icono.jpg")} alt="" className='iconoLogo' />
+        </MDBNavbarBrand>
+        <MDBNavbarToggler
+          type='button'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          onClick={() => setOpenNav(!openNav)}
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
 
+        <MDBCollapse navbar open={openNav}>
+          <MDBNavbarNav>
+            <div className="seccionNavegacion">
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#' className="mx-4 textoNavbar">
+                  Inicio
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#' className="mx-4 textoNavbar">
+                  Contacto
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#' className="mx-4 textoNavbar borderBtn">
+                  Nosotros
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#' className="mx-4 textoNavbar">
+                  Mi cuenta
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+            </div>
+
+            <div className="seccionInput">
+              <MDBNavbarItem>
+                <MDBInput label="Busca Productos" contrast labelStyle={{ color: 'grey' }} type="text" className='textoInputNavbar pe-5 mt-1'>
+                  <i className="fas fa-magnifying-glass trailing px-2"></i>
+                </MDBInput>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#'>
+                  <MDBIcon far icon="heart mt-3 ms-3" size="lg" />
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#'>
+                  <MDBIcon fas icon="shopping-cart mt-3 ms-3" size='lg' />
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+            </div>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
+}
+
+export function NavbarContacto() {
+  const [openNav, setOpenNav] = useState(false);
+
+  return (
+    <MDBNavbar expand='lg' light bgColor='light' className='shadow-0 navbar'>
+      <MDBContainer fluid>
+        <MDBNavbarBrand href='#'>
+          <img src={require("../../images/icono.jpg")} alt="" className='iconoLogo' />
+        </MDBNavbarBrand>
+        <MDBNavbarToggler
+          type='button'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          onClick={() => setOpenNav(!openNav)}
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+
+        <MDBCollapse navbar open={openNav}>
+          <MDBNavbarNav>
+            <div className="seccionNavegacion">
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#' className="mx-4 textoNavbar">
+                  Inicio
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#' className="mx-4 textoNavbar borderBtn">
+                  Contacto
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#' className="mx-4 textoNavbar">
+                  Nosotros
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#' className="mx-4 textoNavbar">
+                  Mi cuenta
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+            </div>
+
+            <div className="seccionInput">
+              <MDBNavbarItem>
+                <MDBInput label="Busca Productos" contrast labelStyle={{ color: 'grey' }} type="text" className='textoInputNavbar pe-5 mt-1'>
+                  <i className="fas fa-magnifying-glass trailing px-2"></i>
+                </MDBInput>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#'>
+                  <MDBIcon far icon="heart mt-3 ms-3" size="lg" />
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#'>
+                  <MDBIcon fas icon="shopping-cart mt-3 ms-3" size='lg' />
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+            </div>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
+}
+
+export function NavbarMiCuenta() {
+  const [openNav, setOpenNav] = useState(false);
+
+  return (
+    <MDBNavbar expand='lg' light bgColor='light' className='shadow-0 navbar'>
+      <MDBContainer fluid>
+        <MDBNavbarBrand href='#'>
+          <img src={require("../../images/icono.jpg")} alt="" className='iconoLogo' />
+        </MDBNavbarBrand>
+        <MDBNavbarToggler
+          type='button'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          onClick={() => setOpenNav(!openNav)}
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+
+        <MDBCollapse navbar open={openNav}>
+          <MDBNavbarNav>
+            <div className="seccionNavegacion">
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#' className="mx-4 textoNavbar">
+                  Inicio
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#' className="mx-4 textoNavbar">
+                  Contacto
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#' className="mx-4 textoNavbar">
+                  Nosotros
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#' className="mx-4 textoNavbar borderBtn">
+                  Mi cuenta
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+            </div>
+
+            <div className="seccionInput">
+              <MDBNavbarItem>
+                <MDBInput label="Busca Productos" contrast labelStyle={{ color: 'grey' }} type="text" className='textoInputNavbar pe-5 mt-1'>
+                  <i className="fas fa-magnifying-glass trailing px-2"></i>
+                </MDBInput>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#'>
+                  <MDBIcon far icon="heart mt-3 ms-3" size="lg" />
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#'>
+                  <MDBIcon fas icon="shopping-cart mt-3 ms-3" size='lg' />
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+            </div>
           </MDBNavbarNav>
         </MDBCollapse>
       </MDBContainer>
--- compartuc/src/index.js
+++ compartuc/src/index.js
@@ -15,7 +15,7 @@ import { CardProducto } from './componentes/cardProducto/cardProducto.jsx';
 import Signup from './componentes/signup/signup.js';
 import Login from './componentes/login/login.js';
 import { Footer } from './componentes/footer/footer.jsx'
-import Navbar from './componentes/navbar/navbar.jsx'
+import { Navbar, NavbarContacto, NavbarNosotros, NavbarMiCuenta } from './componentes/navbar/navbar.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
